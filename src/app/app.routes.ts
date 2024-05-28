bf039fcaa4b53ecfcbe06ac05b7bad0220f6a8fb
IB11 <ibrahimrabai11@gmail.Com>
2024-05-28 10:08:45 +0200

    Init Font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import LevelComponent from './pages/level/level.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "level",
+        loadComponent: () => import("../app/pages/level/level.component")
+    },
+    {
+        path: "home",
+        loadComponent: () => import("../app/home/home.component")
+    },{
+            path: "techno",
+        loadComponent: () => import("../app/pages/technology/technology.component")
+    }
+];
